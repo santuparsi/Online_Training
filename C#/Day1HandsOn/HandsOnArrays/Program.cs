6705eed4d6ab1ea42cd30b3c68e8632cf7d795e1
santuparsi <santuparsi@gmail.com>
2020-08-21 07:08:17 +0530

    initial

---
+++ C#/Day1HandsOn/HandsOnArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HandsOnArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int[] a = new int[5]; //array declaration
+            //assign values
+            a[0] = 12;
+            a[1] = 20;
+            a[2] = 40;
+            Console.WriteLine(i);
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[4]);
+           // Console.WriteLine(a[10]); exception
+            foreach(int k in a)
+            {
+                Console.WriteLine(k);
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Day1HandsOn/HandsOnCSharpBasics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace HandsOnCSharpBasics
+{
+    class Program
+    {
+        double salary = 12000; //class variable
+        static void Main(string[] args)
+        {
+            //value data types
+            //integer types
+            byte b = 10; //local variables
+            short s1 = 32489;
+            int i = 324034;
+            long l = 3428932490;
+            //decimal types
+            double d = 12.345;
+            float f = 12.34f;
+            decimal m = 12.323409834m;
+            m = b * i;
+            //char type
+            char ch = 'a';
+            //bool type
+            bool result = true;
+            int k = 10;
+            //reference types
+            string str = "Hello User";
+            object o = 12;
+            o = 12.34;
+            o = "David";
+            o = new ArrayList();
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
